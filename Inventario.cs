9311d564ac8d7eaef0c3ac3956bf12220b6cb503
candi208 <erandiyoan.candia@upaep.mx>
2026-04-16 08:00:56 -0600

    Inventario.cs

--- Inventario.cs
+++ Inventario.cs
@@ -1,1 +1,22 @@
+using System;
+
+public class Inventario
+{
+  public void AgregarProducto(string nombre, int stock)
+  {
+  }
+  public void ListarProductos()
+  {
+  }
+  public void BuscarProducto(string nombre)
+  {
+  }
+  public void ActualizarStock(int id, int nuevoStock)
+  {
+  }
+  public void EliminarProducto(int id)
+  {
+  }
+      
+}
 
